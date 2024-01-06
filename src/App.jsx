11ee088d83e10e301715035213641e7ceda93fa8
Rayhan426 <rayhan.sarkar704@gmail.com>
2024-01-06 18:47:55 +0600

    third coding

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,15 @@ function App() {
 
   return (
     <>
-     
+
       <h1>Vite + React</h1>
       <Person></Person>
       <Person></Person>
       <Person></Person>
       <Student></Student>
-     
-    
+      <Developer></Developer>
+
+
     </>
   )
 }
@@ -24,24 +25,36 @@ function Person() {
   const age = 25;
   const money = 36;
   const hoby = 'Playing Cricket is my hoby.'
-  const person = {name: 'rayhan', age: 21}
-  return(
+  const person = { name: 'rayhan', age: 21 }
+  return (
     <h3>I am a {person.name} with age: {age + money} and {hoby}</h3>
   )
 }
 
 function Student() {
- return( <div className='student'>
-  <h1>React World!</h1>
-  <p>Name:</p>
-  <p>Age:</p>
-</div>)
+  return (<div className='student'>
+    <h1>React World!</h1>
+    <p>Name:</p>
+    <p>Age:</p>
+    <p>Age:</p>
+    <p>Age:</p>
+  </div>)
 }
 function Developer() {
-  return(
-    <div>
+
+  const developerStyle = {
+    color: 'green',
+    padding: '20px',
+    border: '2px solid red',
+    borderRadius: '20px',
+    marginTop: '30px'
+  }
+  return (
+    <div style={developerStyle}>
       <h5>deve Developer</h5>
       <p>coding</p>
+      <p>coding</p>
+      <p>third coding</p>
     </div>
   )
 }
